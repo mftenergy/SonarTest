e96299262ddbc2e92389737e5e05f435bbf4e0ae
Emil Munksø <ejo@mft-energy.com>
2024-11-18 13:59:27 +0100

    duplicate code

--- app/Program.cs
+++ app/Program.cs
@@ -2,8 +2,57 @@
 Console.WriteLine("Hello, World!");
 
 ComplexMethod();
+ComplexMethod2();
 
 void ComplexMethod()
+{
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int d = 40;
+    int e = 50;
+    int f = 60;
+    int g = 70;
+    int h = 80;
+    int i = 90;
+    int j = 100;
+
+    if (a > b)
+    {
+        if (b > c)
+        {
+            if (c > d)
+            {
+                if (d > e)
+                {
+                    if (e > f)
+                    {
+                        if (f > g)
+                        {
+                            if (g > h)
+                            {
+                                if (h > i)
+                                {
+                                    if (i > j)
+                                    {
+                                        Console.WriteLine("a > b > c > d > e > f > g > h > i > j");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (int k = 0; k < 100; k++)
+    {
+        Console.WriteLine("This is line " + k);
+    }
+}
+
+void ComplexMethod2()
 {
     int a = 10;
     int b = 20;
